f2aa1ebace3357e718fe9b4401e44b23b251d14b
Lev Taydakov <lev.taydakov@gmail.com>
2014-06-24 19:58:45 -0700

    bug of } fixed

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -6,4 +6,5 @@ node default {
     managehome => true,
     password => '$6$lY2Gp3Cr$zNrUB7T3yibUF/gWn5cTQ0fNv7MUmx/DZuw3E7I..Vh9tITG28BtgvXJPU4Gm4Z/9oNvlbX24KzQ9Ib1QH1B9.',
     shell => '/bin/bash',
+  }
 }
