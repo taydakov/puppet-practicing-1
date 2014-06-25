38bf6c90ea280b80becc124633310a6a2a937aa4
Lev Taydakov <lev.taydakov@gmail.com>
2014-06-24 19:57:37 -0700

    manage user levka

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,9 @@
 node default {
-
+  user { 'levka':
+    ensure => 'present',
+    groups => ['sudo'],
+    home => '/home/levkahome',
+    managehome => true,
+    password => '$6$lY2Gp3Cr$zNrUB7T3yibUF/gWn5cTQ0fNv7MUmx/DZuw3E7I..Vh9tITG28BtgvXJPU4Gm4Z/9oNvlbX24KzQ9Ib1QH1B9.',
+    shell => '/bin/bash',
 }
