892d8f216a2977aa676c8def117b82e0c649d02a
Lev Taydakov <lev.taydakov@gmail.com>
2014-06-24 20:17:45 -0700

    group ubuntu added and levka is assigned to it

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,10 +1,13 @@
 node default {
   user { 'levka':
     ensure => 'present',
-    groups => ['sudo'],
+    groups => ['sudo', 'ubuntu'],
     home => '/home/levkahome',
     managehome => true,
     password => '$1$vb1tLY1q$y9/uOADRI2EL1bXIAD49k0', # levka
     shell => '/bin/bash',
+  } ->
+  user { 'ubuntu':
+    ensure => 'present',
   }
 }
