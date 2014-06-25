ab0a746a3e96271d3d8425d2173fe9b555f6f7f9
Lev Taydakov <lev.taydakov@gmail.com>
2014-06-24 20:12:23 -0700

    comment added to password

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -4,7 +4,7 @@ node default {
     groups => ['sudo'],
     home => '/home/levkahome',
     managehome => true,
-    password => '$6$lY2Gp3Cr$zNrUB7T3yibUF/gWn5cTQ0fNv7MUmx/DZuw3E7I..Vh9tITG28BtgvXJPU4Gm4Z/9oNvlbX24KzQ9Ib1QH1B9.',
+    password => '$6$lY2Gp3Cr$zNrUB7T3yibUF/gWn5cTQ0fNv7MUmx/DZuw3E7I..Vh9tITG28BtgvXJPU4Gm4Z/9oNvlbX24KzQ9Ib1QH1B9.', # test
     shell => '/bin/bash',
   }
 }
